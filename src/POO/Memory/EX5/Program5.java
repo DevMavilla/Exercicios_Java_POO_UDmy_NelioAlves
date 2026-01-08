f8695a636f8a818bc973ef6f7c15cace717563fa
Diego Avila <diegomoraesavilla99@gmail.com>
2026-01-07 22:23:45 -0300

    Reorganização projetos

---
+++ src/POO/Memory/EX5/Program5.java
@@ -0,0 +1,40 @@
+package POO.Memory.EX5;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program5 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos números vcê vai digitar? ");
+        int n = sc.nextInt();
+        double soma, media;
+
+        double vect[] = new double[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Digite um número: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        soma =0;
+        for (int i = 0; i < n; i++) {
+            soma  += vect[i];
+        }
+
+        media = soma / n;
+
+        System.out.print("valores = ");
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.printf("%.1f " , vect[i]);
+        }
+
+        System.out.printf("\nSoma = %.2f%n" , soma);
+        System.out.printf("Média = %.2f%n" ,media);
+
+        sc.close();
+    }
+}
---
+++ src/POO/Memory/POOEX4/Programa4.java
@@ -0,0 +1,32 @@
+package POO.Memory.POOEX4;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa4 {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+        System.out.print("Digite o nome do funcionário: ");
+        employee.name = sc.nextLine();
+
+        System.out.print("Digite o salário bruto do funcionário: ");
+        employee.grossSalary = sc.nextDouble();
+
+        System.out.print("Digite a taxa do funcionário: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println("employee: " + employee.name + ", $ " + employee.netSalary());
+
+        System.out.print("Digite um valor de aumento do salário do funcionário: ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println("Upsated data: " + employee);
+
+        sc.close();
+    }
+}
