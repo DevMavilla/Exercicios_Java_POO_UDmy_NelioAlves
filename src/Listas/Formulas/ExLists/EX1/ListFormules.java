97a0cd09a4eaa2affaddf8d331e27093c91e5efc
Diego Avila <diegomoraesavilla99@gmail.com>
2026-01-09 17:58:16 -0300

    EX1 LIST COMPLETE

--- src/Listas/Formulas/ExLists/EX1/ListFormules.java
+++ src/Listas/Formulas/ExLists/EX1/ListFormules.java
@@ -1,4 +1,4 @@
-package Listas.Formulas;
+package Listas.Formulas.ExLists.EX1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -6,52 +6,52 @@ import java.util.stream.Collectors;
 public class ListFormules {
     public static void main(String[] args){
 
-        /* CRIANDO UMA LIST DO TIPO <STRING> NOME: list = UTILIZANDO A INTERFACE ArrayList(); */
+        // CRIANDO UMA LIST DO TIPO <STRING> NOME: list = UTILIZANDO A INTERFACE ArrayList();
         List<String> list = new ArrayList<>();
 
-        /* ADICIONANDO ITENS NA LISTA CONFORME NECESSIDADE*/
+        // ADICIONANDO ITENS NA LISTA CONFORME NECESSIDADE
         list.add("Maria");
         list.add("Alex");
         list.add("Bob");
         list.add("Anna");
         list.add(2, "Marco");
 
-        /* MOSTRA TAMANHO DA LISTA - .size*/
+        // MOSTRA TAMANHO DA LISTA - .size*/
         System.out.println(list.size());
         System.out.println("---------------------------");
 
-        /* PERCORRE LISTA E EXIBE SEUS ELEMENTOS USANDO LOOP FOREACH FOR(TIPO NOME : ListaQueSeraPercorrida*/
+        // PERCORRE LISTA E EXIBE SEUS ELEMENTOS USANDO LOOP FOREACH FOR(TIPO NOME : ListaQueSeraPercorrida
         for(String x : list) {
             System.out.println(x);
         }
         System.out.println("---------------------------");
 
-        /* REMOVE ITENS QUE INICIEM COM LETRA X DEFINIDA */
+        // REMOVE ITENS QUE INICIEM COM LETRA X DEFINIDA
         list.removeIf(x -> x.charAt(0) == 'M');
         for (String x : list) {
             System.out.println(x);
         }
         System.out.println("---------------------------");
 
-        /* BUSCA POSIÇÃO DO OBJ NA LISTA .indexOf */
+        // BUSCA POSIÇÃO DO OBJ NA LISTA .indexOf
         System.out.println("Index Of Bob: " + list.indexOf("Bob"));
         System.out.println("Index Of Bob: " + list.indexOf("Marco"));
         System.out.println("---------------------------");
 
-        /* FILTRA LISTA E EXIBE APENAS OBJ COM A INICIAL X ESCOLHIDA*/
+        // FILTRA LISTA E EXIBE APENAS OBJ COM A INICIAL X ESCOLHIDA*/
+        //Primeiro converte para Stream* - faz a operação filtro com expressão Lambda que precisar .filter(...)
+        // - Depois converter para lista collect(c.Tolist()).../
         List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
         for (String x : result){
             System.out.println(x);
         }
         System.out.println("---------------------------");
-        /*Primeiro converte para Stream* - faz a operação Lambda que precisar .filter(...) - Depois converter para lista collect.../ */
 
-        /* ENCONTRA ELEMENTO COM PREDICADO X : PRIMEIRO ELEMETO COM A LETRA A FindFirst ou null (orElse)*/
-
-        /* ENCONTRA O ELEMENTO */
+        // ENCONTRA ELEMENTO COM PREDICADO X : PRIMEIRO ELEMENTO COM A LETRA A FindFirst ou null (orElse)
+        // ENCONTRA O ELEMENTO
         String name = list.stream().filter( x -> x.charAt(0) == 'A').findFirst().orElse(null);
 
-        /* DEVOOLVE NULL CASO NÃO ENCONTRE */
+        // DEVOLVE NULL CASO NÃO ENCONTRE
         String names = list.stream().filter( x -> x.charAt(0) == 'j').findFirst().orElse(null);
         System.out.println(name);
         System.out.println(names);
---
+++ src/Listas/Formulas/ExLists/EX1/entities/Employee.java
@@ -0,0 +1,62 @@
+package Listas.Formulas.ExLists.EX1.entities;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Employee {
+    private Integer id;
+    private String name;
+    private Double salary;
+
+
+
+    public Employee(String name, Integer id, Double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100.0;
+    }
+
+
+    @Override
+    public String toString() {
+        return  id + ", "+ name + ", " + salary + '}';
+    }
+
+
+}
+
+
+
+
+
+
+
+
