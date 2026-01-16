3e578be46068c955b4c65545f4f7a1ad6c56766d
Diego Avila <diegomoraesavilla99@gmail.com>
2026-01-15 22:09:32 -0300

    Exercício final - COMPOSIÇÂO

--- src/Composicao/ExercicioFinal/entities/Client.java
+++ src/Composicao/ExercicioFinal/entities/Client.java
@@ -1,8 +1,11 @@
 package Composicao.ExercicioFinal.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd//MM//yyyy");
+
     private String name;
     private String email;
     private Date birthDate;
@@ -38,5 +41,10 @@ public class Client {
     }
 
     public Client() {
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + sdf.format(birthDate) + ") - " + email;
     }
 }
--- src/Composicao/ExercicioFinal/entities/OrderItem.java
+++ src/Composicao/ExercicioFinal/entities/OrderItem.java
@@ -3,12 +3,15 @@ package Composicao.ExercicioFinal.entities;
 public class OrderItem {
     private Integer quantity;
     private Double price;
-    private Product name;
 
-    public OrderItem(Integer quantity, Double price, Product name) {
+    private Product product;
+
+
+
+    public OrderItem(Integer quantity, Double price, Product product) {
         this.quantity = quantity;
         this.price = price;
-        this.name = name;
+        this.product = product;
     }
 
     public Integer getQuantity() {
@@ -30,7 +33,26 @@ public class OrderItem {
     public OrderItem() {
     }
 
-    public void subTotal(Double price, Integer quantity){
-        Double subTotal = price * quantity;
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double subTotal(){
+        return price * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return getProduct().getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity: "
+                + quantity
+                + ", Subtotal: $"
+            + String.format("%.2f", subTotal());
     }
 }
